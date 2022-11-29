5695a1cad3d3175283b5dbaafdc47545718fba84
KonstiHe <79403490+KonstiHe@users.noreply.github.com>
2022-11-29 19:34:30 +0100

    Updating App.js

--- src/App.js
+++ src/App.js
@@ -94,7 +94,7 @@ class App extends Component {
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
         <NumberOfEvents numberOfEvents={this.state.numberOfEvents} updateNumberOfEvents={this.updateNumberOfEvents} />
         <div className="data-vis-wrapper">
-          <EventGenre events={events} />
+          <EventGenre events={this.state.events} />
           <ResponsiveContainer height={400} >
             <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
               <CartesianGrid />
